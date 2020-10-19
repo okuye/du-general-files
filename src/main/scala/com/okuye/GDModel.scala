850af99d5e96b48543eca02d9d7b686ab96ecdc1
Olakunle Kuye <olakuye@gmail.com>
2020-10-19 02:20:23 +0100

    Added logic to catch exceptions

--- src/main/scala/com/okuye/GDModel.scala
+++ src/main/scala/com/okuye/GDModel.scala
@@ -58,104 +58,109 @@ object GDModel {
   //Return the count of data from an s3 directory
   def returnDataCount(opType: String, inputPath: String, aws_access_key_id: String, aws_secret_access_key: String): Long = {
 
-    spark.sparkContext
-      .hadoopConfiguration.set("fs.s3a.access.key", aws_access_key_id)
-    // Replace Key with your AWS secret key (You can find this on IAM
-    spark.sparkContext
-      .hadoopConfiguration.set("fs.s3a.secret.key", aws_secret_access_key)
-    spark.sparkContext
-      .hadoopConfiguration.set("fs.s3a.endpoint", "s3.amazonaws.com")
+      spark.sparkContext
+        .hadoopConfiguration.set("fs.s3a.access.key", aws_access_key_id)
+      // Replace Key with your AWS secret key (You can find this on IAM
+      spark.sparkContext
+        .hadoopConfiguration.set("fs.s3a.secret.key", aws_secret_access_key)
+      spark.sparkContext
+        .hadoopConfiguration.set("fs.s3a.endpoint", "s3.amazonaws.com")
 
-    //Load a datafrane with the content of the file and return the count
-    val df = spark.read.option("sep", getDelimiter(opType)).option("header", "false").csv(inputPath)
+      //Load a datafrane with the content of the file and return the count
+      val df = spark.read.option("sep", getDelimiter(opType)).option("header", "false").csv(inputPath)
 
-    df.count()
+      df.count()
+
   }
 
   def writeData(opType: String, inputPath: String, outputPath: String, aws_access_key_id: String, aws_secret_access_key: String): Unit = {
 
+    try {
+      //Deteremine the extension type of the file
+      val extType = getDelimiter(getFileType(inputPath))
 
-    //Deteremine the extension type of the file
-    val extType = getDelimiter(getFileType(inputPath))
+      //Load a datafrane with the content of the file and replace all null values with 0
+      val segments = spark.read.option("sep", extType).option("header", "false").csv(inputPath)
+        .na.fill(0)
 
-    //Load a datafrane with the content of the file and replace all null values with 0
-    val segments = spark.read.option("sep", extType).option("header", "false").csv(inputPath)
-      .na.fill(0)
+      //Determine and Remove the inconsistent header(s) present in the data
+      val header = segments.first()
 
-    //Determine and Remove the inconsistent header(s) present in the data
-    val header = segments.first()
+      val tempDataNoHeader = segments.filter(line => line != header)
 
-    val tempDataNoHeader = segments.filter(line => line != header)
+      //Rename the inconsistent columns to key and value
+      val tempDataNoHeader_01 = tempDataNoHeader.withColumnRenamed(tempDataNoHeader.columns(0), "key")
+        .withColumnRenamed(tempDataNoHeader.columns(1), "value")
+      val tempDataNoHeader_02 = tempDataNoHeader_01.withColumn("key", tempDataNoHeader_01("key")
+        .cast(sql.types.IntegerType)).withColumn("value", tempDataNoHeader_01("value")
+        .cast(sql.types.IntegerType))
 
-    //Rename the inconsistent columns to key and value
-    val tempDataNoHeader_01 = tempDataNoHeader.withColumnRenamed(tempDataNoHeader.columns(0), "key")
-      .withColumnRenamed(tempDataNoHeader.columns(1), "value")
-    val tempDataNoHeader_02 = tempDataNoHeader_01.withColumn("key", tempDataNoHeader_01("key")
-      .cast(sql.types.IntegerType)).withColumn("value", tempDataNoHeader_01("value")
-      .cast(sql.types.IntegerType))
+      // Creates a temporary view using the DataFrame
+      tempDataNoHeader_02.createOrReplaceTempView("tempDataNoHeader_02")
 
-    // Creates a temporary view using the DataFrame
-    tempDataNoHeader_02.createOrReplaceTempView("tempDataNoHeader_02")
+      //Determine the distint keys in the data
+      val distinctKeys = spark.sql("select distinct key from tempDataNoHeader_02")
 
-    //Determine the distint keys in the data
-    val distinctKeys = spark.sql("select distinct key from tempDataNoHeader_02")
+      //Drop the key column as is not required to filter the data
+      val tempDataNoHeader_03 = tempDataNoHeader_02.drop("key")
 
-    //Drop the key column as is not required to filter the data
-    val tempDataNoHeader_03 = tempDataNoHeader_02.drop("key")
+      import spark.implicits._
 
-    import spark.implicits._
+      //Create a collection to hold the key value pairs
+      var tempKeyValuesMap = scala.collection.mutable.Map[Int, Int]()
 
-    //Create a collection to hold the key value pairs
-    var tempKeyValuesMap = scala.collection.mutable.Map[Int, Int]()
+      //Iterate over the distinct values in order to determine the odd occurrences in the main dataset
+      val tDa = distinctKeys.collect()
+      for (row <- tDa) {
 
-    //Iterate over the distinct values in order to determine the odd occurrences in the main dataset
-    val tDa = distinctKeys.collect()
-    for (row <- tDa) {
+        //Get each unique key from the distinct keys in the data
+        val tempVal = row.getInt(0)
 
-      //Get each unique key from the distinct keys in the data
-      val tempVal = row.getInt(0)
+        val keyFilteredData = tempDataNoHeader_03.filter("value = " + tempVal)
 
-      val keyFilteredData = tempDataNoHeader_03.filter("value = " + tempVal)
+        val keyFilteredDataArray = keyFilteredData.select("value").map(_.getInt(0)).collect()
 
-      val keyFilteredDataArray = keyFilteredData.select("value").map(_.getInt(0)).collect()
-
-      //Create a map of a key and value that has odd occurences in the value column
-      tempKeyValuesMap += (tempVal -> getOddOccurrence(keyFilteredDataArray))
+        //Create a map of a key and value that has odd occurences in the value column
+        tempKeyValuesMap += (tempVal -> getOddOccurrence(keyFilteredDataArray))
 
 
+      }
+
+
+      spark.sparkContext
+        .hadoopConfiguration.set("fs.s3a.access.key", aws_access_key_id)
+      // Replace Key with your AWS secret key (You can find this on IAM
+      spark.sparkContext
+        .hadoopConfiguration.set("fs.s3a.secret.key", aws_secret_access_key)
+      spark.sparkContext
+        .hadoopConfiguration.set("fs.s3a.endpoint", "s3.amazonaws.com")
+
+      //Convert the collection into a dataframe to make saving easier
+      val df = tempKeyValuesMap.toSeq.toDF()
+
+      opType.toLowerCase match {
+        case "csv" =>
+          //Save the output to a directory
+          df.coalesce(1)
+            .write
+            .mode("overwrite")
+            .option("sep", ",")
+            .option("encoding", "UTF-8")
+            .csv(outputPath)
+        case "tsv" =>
+          //Save the output to a directory
+          df.coalesce(1)
+            .write
+            .mode("overwrite")
+            .option("sep", "\t")
+            .option("encoding", "UTF-8")
+            .csv(outputPath)
+        case _ => println("The options to save are either csv or tsv")
+
+      }
     }
-
-
-    spark.sparkContext
-      .hadoopConfiguration.set("fs.s3a.access.key", aws_access_key_id)
-    // Replace Key with your AWS secret key (You can find this on IAM
-    spark.sparkContext
-      .hadoopConfiguration.set("fs.s3a.secret.key", aws_secret_access_key)
-    spark.sparkContext
-      .hadoopConfiguration.set("fs.s3a.endpoint", "s3.amazonaws.com")
-
-    //Convert the collection into a dataframe to make saving easier
-    val df = tempKeyValuesMap.toSeq.toDF()
-
-    opType.toLowerCase match {
-      case "csv" =>
-        //Save the output to a directory
-        df.coalesce(1)
-          .write
-          .mode("overwrite")
-          .option("sep", ",")
-          .option("encoding", "UTF-8")
-          .csv(outputPath)
-      case "tsv" =>
-        //Save the output to a directory
-        df.coalesce(1)
-          .write
-          .mode("overwrite")
-          .option("sep", "\t")
-          .option("encoding", "UTF-8")
-          .csv(outputPath)
-      case _ => println("The options to save are either csv or tsv")
-
+    catch {
+      case e: Exception => e.printStackTrace
     }
   }
 }
--- src/main/scala/com/okuye/Main.scala
+++ src/main/scala/com/okuye/Main.scala
@@ -7,18 +7,24 @@ object Main {
 
   def main(args: Array[String]): Unit = {
 
-    args.foreach(arg => if (arg.length == 0) die()) //Exit the application if all the parameters are not supplied
+    try {
 
-    val inputPath = args(0)
-    val outputPath = args(1)
+      args.foreach(arg => if (arg.length == 0) die()) //Exit the application if all the parameters are not supplied
 
-    val awsCredentials = getCredentials(args(2))
-    val aws_access_key_id = awsCredentials("aws_access_key_id")
-    val aws_secret_access_key = awsCredentials("aws_secret_access_key")
+      val inputPath = args(0)
+      val outputPath = args(1)
 
-    //Write the output as a csv for a datafie with unique key and value indicating the odd occurrence
-    writeData("csv", inputPath, outputPath, aws_access_key_id, aws_secret_access_key)
+      val awsCredentials = getCredentials(args(2))
+      val aws_access_key_id = awsCredentials("aws_access_key_id")
+      val aws_secret_access_key = awsCredentials("aws_secret_access_key")
 
+      //Write the output as a csv for a datafie with unique key and value indicating the odd occurrence
+      writeData("csv", inputPath, outputPath, aws_access_key_id, aws_secret_access_key)
+
+    }
+    catch {
+      case e: Exception => e.printStackTrace
+    }
 
   }
 
